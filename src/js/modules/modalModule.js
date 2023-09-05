3e71fcf221ab73f8befff6c54d3b4bed11deeb9a
Maksym Huk <maksim.guk@gmail.com>
2023-09-05 14:07:19 +0300

    add wrightToUsModal JS, html

--- src/js/modules/modalModule.js
+++ src/js/modules/modalModule.js
@@ -1,11 +1,71 @@
-// Функція для відкриття модального вікна
-export function openModal() {
-  let modal = document.getElementById('feedbackModal');
-  modal.style.display = 'block';
+let wrightToUsModal = document.getElementById('wrightToUsModal');
+let feedbackModal = document.getElementById('feedbackModal');
+
+// Відкриття модального вікна "wrightToUsModal"
+function openWrightToUsModal() {
+  wrightToUsModal.style.display = 'block';
 }
 
-// Функція для закриття модального вікна
-export function closeModal() {
-  let modal = document.getElementById('feedbackModal');
-  modal.style.display = 'none';
+// Закриття модального вікна "wrightToUsModal"
+function closeWrightToUsModal() {
+  wrightToUsModal.style.display = 'none';
+  document.getElementById('wright-name').value = '';
+  document.getElementById('wright-phone').value = '';
+  document.getElementById('wright-email').value = '';
 }
+
+// Відкриття модального вікна "feedbackModal"
+function openFeedbackModal() {
+  feedbackModal.style.display = 'block';
+}
+
+// Закриття модального вікна "feedbackModal"
+function closeFeedbackModal() {
+  feedbackModal.style.display = 'none';
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('phone').value = '';
+}
+
+
+// Функція для ініціалізації модальних вікон
+function modalInit() {
+  const openWrightToUsButton = document.querySelector('.openWrightToUsButton');
+  const openFeedbackButton = document.querySelector('.openFeedbackButton');
+  const openFeedbackButton2 = document.querySelector('.openFeedbackButton2');
+
+  openWrightToUsButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    openWrightToUsModal();
+  });
+  document.querySelector('.closeWrightToUsButton').addEventListener('click', function (e) {
+    // document.getElementById('wright-name').value = '';
+    // document.getElementById('wright-phone').value = '';
+    // document.getElementById('wright-email').value = '';
+    closeWrightToUsModal();
+  });
+
+  openFeedbackButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    openFeedbackModal();
+  });
+
+  openFeedbackButton2.addEventListener('click', function (e) {
+    e.preventDefault();
+    // document.getElementById('wright-name').value = '';
+    // document.getElementById('wright-phone').value = '';
+    // document.getElementById('wright-email').value = '';
+    closeWrightToUsModal();
+    openFeedbackModal();
+  });
+  document.querySelector('.closeFeedbackButton').addEventListener('click', function (e) {
+    // document.getElementById('name').value = '';
+    // document.getElementById('email').value = '';
+    // document.getElementById('phone').value = '';
+    closeFeedbackModal();
+  });
+}
+
+export {
+  modalInit
+};
